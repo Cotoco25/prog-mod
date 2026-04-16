ab419dac33b768b7e34a9fa91cf7c24f6ab5ba2d
Cotoco25 <161837492+Cotoco25@users.noreply.github.com>
2026-04-16 12:53:58 -0300

    Add files via upload

---
+++ gol_random _contagem_partida.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 16 12:46:59 2026
+
+@author: PC37
+"""
+
+import random
+
+def exibeJogos(g1,g2,g3,g4,g5,g6,a1,a2,a3,a4,a5,a6):
+    print("---Partida 1---")
+    print("OLIMPO (g1) X Adv1 (a1):"), if g1>a1: print("OLIMPO VENCEU!"), else: print("OLIMPO PERDEU!")
+
+
+g1 = random.randit(0,7)
+g2 = random.randit(0,7)
+g3 = random.randit(0,7)
+g4 = random.randit(0,7)
+g5 = random.randit(0,7)
+g6 = random.randit(0,7)
+a1 = random.randit(0,7)
+a2 = random.randit(0,7)
+a3 = random.randit(0,7)
+a4 = random.randit(0,7)
+a5 = random.randit(0,7)
+a6 = random.randit(0,7)
