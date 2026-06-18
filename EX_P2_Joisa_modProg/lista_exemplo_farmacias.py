a9bef8134fe2aa63307b49d7c23e4847823ac8e8
grrt <161837492+Cotoco25@users.noreply.github.com>
2026-06-18 12:34:38 -0300

    Add files via upload

---
+++ EX_P2_Joisa_modProg/lista_exemplo_farmacias.py
@@ -0,0 +1,10 @@
+
+lfarmacias= [['FARMMAH', [ ['BomCheiro',9.20,150], ['Limpex',8.50,18],  ['Glicerinix',5.92,30], ['NutriMilk',6.90,80] ] ],
+             ['KIKKAS',  [ ['CleanRose',6.75,40],  ['BomCheiro',7.25,200], ['RosaBranca',9.0, 50] ] ],
+             ['CANOINHA',[ ['NutriMilk',5.83,20] , ['CleanRose',8.15,120] ] ] ,
+             ['DROGGHY', [ ['CleanRose',4.1,35],  ['BomCheiro',7.75, 60], ['RosaBranca',8.35, 40] ] ],
+             ['PHAPHAH', [ ['CleanRose',6.1,85],  ['Glicerinix',5.80, 40],['BomCheiro',6.75, 60] ] ],
+             ['FINAFARM',[ ['Limpex',7.87,20]  ] ] ]
+
+
+        
